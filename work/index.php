d971e2367e92f9d9ba4c6649ca0e14000e32a4a2
keima-d08 <100249962+keima-d08@users.noreply.github.com>
2022-05-04 23:13:33 +0900

    Update index.php

--- work/index.php
+++ work/index.php
@@ -1,3 +1,15 @@
+<?php 
+$n = mt_rand(1, 3);
+
+if ($n === 1) {
+  $message = 'UI/UXデザイナー見習いです';
+} elseif ($n === 2) {
+  $message = 'プログラミング勉強中です';
+} else {
+  $message = 'PHPちょっとわかります';
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -15,7 +27,7 @@
       </div>
       <div class="info">
         <h1>山田太郎</h1>
-        <p><?php echo 'こんばんわ!' . date('l'); ?></p>
+        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
         <ul>
           <li>
             <a href="https://dotinstall.com" target="_blank">
